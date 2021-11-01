ca1e08df9672cbe479a290cfa3e0a39330fd32d3
= <=>
2021-11-01 19:59:17 +0900

    Initial commit

---
+++ src/main/java/com/starlab/msa/personalinfo/PersonalinfoApplication.java
@@ -0,0 +1,13 @@
+package com.starlab.msa.personalinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starlab/msa/personalinfo/PersonalinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlab.msa.personalinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
